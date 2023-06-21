d2bd220673001020beed1a4c1ea1c0a4082d1246
Tiago Vasconcelos <talvasconcelos@gmail.com>
2023-06-21 10:05:09 +0100

    fix latest fastapi
    https://github.com/lnbits/lnbits/pull/1609

--- views_api.py
+++ views_api.py
@@ -85,7 +85,7 @@ async def api_form_delete(event_id, wallet: WalletTypeInfo = Depends(get_key_typ
 
 @events_ext.get("/api/v1/tickets")
 async def api_tickets(
-    all_wallets: bool = Query(False), wallet: WalletTypeInfo = Depends(get_key_type)
+    all_wallets: bool, wallet: WalletTypeInfo = Depends(get_key_type)
 ):
     wallet_ids = [wallet.wallet.id]
 
