0824b1120b0d0431576b98ddb2ff18b0f1308d20
dni ⚡ <office@dnilabs.com>
2026-05-07 17:06:38 +0200

    feat: add paid/registered badge to ticket page (#49)
    some visual verification on the ticket page that it is paid / checked
    in.

--- static/js/ticket.vue
+++ static/js/ticket.vue
@@ -5,20 +5,32 @@
         <q-card-section class="q-pa-none">
           <center>
             <h3 class="q-my-none">Ticket</h3>
+            <h5 v-if="ticket" v-text="ticket.name" class="q-my-none"></h5>
             <br />
             <h5 class="q-my-none">
               Bookmark, print or screenshot this page,<br />
               and present it for registration!
             </h5>
-            <br />
+            <div v-if="ticket" class="row justify-center q-gutter-sm q-mb-md">
+              <q-btn
+                unelevated
+                :color="ticket.paid ? 'positive' : 'negative'"
+                :label="ticket.paid ? 'Paid' : 'Not Paid'"
+              ></q-btn>
+              <q-btn
+                unelevated
+                :color="ticket.registered ? 'positive' : 'warning'"
+                :label="ticket.registered ? 'Checked In' : 'Not Checked In'"
+              ></q-btn>
+            </div>
             <lnbits-qrcode
               :value="`ticket://${ticketId}`"
               :options="{width: 500}"
             ></lnbits-qrcode>
             <br />
-            <q-btn @click="printWindow" color="grey" class="q-ml-auto">
-              <q-icon left size="3em" name="print"></q-icon> Print</q-btn
-            >
+            <q-btn @click="printWindow" color="grey">
+              <q-icon left size="3em" name="print"></q-icon> Print
+            </q-btn>
           </center>
         </q-card-section>
       </q-card>
